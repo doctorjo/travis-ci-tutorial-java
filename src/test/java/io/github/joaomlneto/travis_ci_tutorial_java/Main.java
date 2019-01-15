45000660f90f4231c3db8252509e76e8e4475543
doctorjo <jojo.grundy@gmail.com>
2019-01-15 11:12:30 +0000

    added main

---
+++ src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/Main.java
@@ -0,0 +1,9 @@
+package io.github.joaomlneto.travis_ci_tutorial_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleCalculator calc = new SimpleCalculator();
+        System.out.println(calc.add(3,2));
+    }
+}
